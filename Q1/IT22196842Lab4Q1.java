7c632ebba051c70845f83e01a9c3ebf663e2c674
it22196842 <it22196842@my.sliit.lk>
2026-09-07 18:06:50 +0530

    Add IT22196842Lab4Q1.java to Q1

---
+++ Q1/IT22196842Lab4Q1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+	public class IT22196842Lab4Q1 {
+		public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	
+	
+	System.out.print("Enter a number: ");
+	double num1 = input.nextDouble();
+ 	
+	if(num1>0)
+            {
+		System.out.println("The number is : Positive");
+           }
+	else if(num1<0)
+		{
+		System.out.println("The number is : Negative");
+		}
+	else{
+	        System.out.println("The number  is Zero");
+	
+	}
+}
+}
+	
+ 
+	
